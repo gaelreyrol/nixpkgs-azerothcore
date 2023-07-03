88199a7705fc2b3dc96031564589609c3032e9c8
Gaël Reyrol <me@gaelreyrol.dev>
2023-07-03 23:46:44 +0200

    feat: add azerothcore-wotlk derivation

---
+++ default.nix
@@ -0,0 +1,45 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+, mariadb_106
+, git
+, boost
+, openssl
+, zlib
+, readline
+, bzip2
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "azerothcore-wotlk";
+  version = "unstable-2023-07";
+
+  src = fetchFromGitHub {
+    owner = "azerothcore";
+    repo = "azerothcore-wotlk";
+    rev = "21cab042328d304e968caa79d87e682da45a24a6";
+    hash = "sha256-Wc8PgmfMCCbp4Tv9BB16txIrMrk8KzrRlpDyycPwyCs=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    mariadb_106
+    git
+    boost
+    openssl
+    zlib
+    readline
+    bzip2
+  ];
+
+  cmakeFlags = [ "-DCMAKE_INSTALL_PREFIX=$out" "-DTOOLS_BUILD=all" "-DSCRIPTS=static" "-DMODULES=static" ];
+
+  meta = with lib; {
+    description = "Complete Open Source and Modular solution for MMO";
+    homepage = "http://www.azerothcore.org/";
+    license = licenses.agpl3;
+    platforms = [ "x86_64-linux" ];
+    maintainers = with maintainers; [ gaelreyrol ];
+  };
+})
---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs }: 
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    packages.${system}.default = pkgs.callPackage ./default.nix { };
+  };
+}
